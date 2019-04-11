6d2069e637c762e54b2764ce192576f7a94642ec
jaimariev <jaimarievelasco@ymail.com>
2019-04-10 22:58:26 -0700

    added other html files

---
+++ js/info.js
@@ -0,0 +1,88 @@
+var info = {
+  "results": [ 
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 1,
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 2
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 3
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 4
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 5
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 6
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 7
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 8
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 9
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 10
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 11
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 12
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 13
+    },
+    {
+    "name": "",
+    "about": "",
+    "birthday": "",
+    "id": 14
+    }
+  ]
+}
